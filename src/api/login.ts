c2f6f780e64a5fe58a0a0b246fe106343e6e7a08
Rhino-ty <okko8522@gmail.com>
2023-03-09 20:58:35 +0900

    2-2 과제 수행

--- src/api/login.ts
+++ src/api/login.ts
@@ -84,6 +84,20 @@ export const login = async (args: LoginRequest): Promise<LoginResult> => {
   // POST, `${ BASE_URL }/auth/login`을 호출하세요.
   // API Spec은 강의 자료를 참고하세요.
   // access_token 발급에 성공한 경우에는 saveAccessTokenToLocalStorage 함수를 호출하여 access_token을 localStorage에 저장하고 'success'를 반환하세요.
+  const loginResponse = await fetch(`${BASE_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(args),
+  });
+
+  const loginResponseData = await loginResponse.json();
+  saveAccessTokenToLocalStorage(loginResponseData.access_token);
+
+  if (loginResponse.ok) {
+    return "success";
+  }
 
   return "fail";
 };
@@ -94,6 +108,18 @@ export const getCurrentUserInfo = async (): Promise<UserInfo | null> => {
   // 로컬 스토리지에 있는 token을 getAccessTokenFromLocalStorage로 가져와서 Authorization header에 Bearer token으로 넣어주세요.
   // API Spec은 강의 자료를 참고하세요.
   // 유저 정보 조회에 성공한 경우에는 UserInfo 타입의 값을 반환하세요.
+
+  const userInfoRes = await fetch(`${BASE_URL}/profile`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getAccessTokenFromLocalStorage()}`,
+    },
+  });
+
+  if (userInfoRes.ok) {
+    return userInfoRes.json() as Promise<UserInfo>;
+  }
 
   return null;
 };
